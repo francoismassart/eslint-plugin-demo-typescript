a8c27f712af47eb76776a30b7d46c24572b073ee
francoismassart <francois.massart@gmail.com>
2024-06-12 15:17:53 +0200

    chore: add the code as in the Medium article

---
+++ README.md
@@ -0,0 +1,31 @@
+# Writing a custom ESLint plugin with TypeScript code
+
+This repo is based on the "[Writing a custom ESLint plugin with Typescript](https://thesonofthomp.medium.com/writing-a-custom-eslint-plugin-with-typescript-08d0e01726d2)" Medium article written by [Adam Thompson
+](https://github.com/thesonofthomp).
+
+## Why this repo?
+
+I was expecting to find a link to a repo with the full code explained in Adam's article but I could not find it.
+
+It will be a quick way to browse the full code on its own and possibly to enhance it by fixing issues.
+
+## Getting started
+
+### Install
+
+In this repo's folder:
+`pnpm i`
+
+#### Build (Not working yet)
+
+`pnpm build`
+
+#### Test (Not working yet)
+
+`pnpm test`
+
+## Docs (Not working yet)
+
+`npx eslint-doc-generator --init-rule-docs`
+
+`npx eslint-doc-generator`
---
+++ lib/rules/my-rule.ts
@@ -0,0 +1,46 @@
+import { TSESTree } from '@typescript-eslint/types';
+export { ESLintUtils } from '@typescript-eslint/utils';
+
+// The Rule creator returns a function that is used to create a well-typed ESLint rule
+// The parameter passed into RuleCreator is a URL generator function.
+export const createRule = ESLintUtils.RuleCreator(name => `https://my-website.io/eslint/${name}`);
+
+export const myRule= createRule({
+  name: 'my-rule',
+  meta: {
+    docs: {
+      description: 'An example ESLint rule',
+    },
+    type: 'suggestion',
+    messages: {
+      // Message IDs don't need to be prefixed, I just find it easier to keep track of them this way
+      'issue:var': 'Prefer using `let` or `const`',
+      'fix:let': 'Replace this `var` declaration with `let`',
+      'fix:const': 'Replace this `var` declaration with `const`',
+    },
+    schema: [],
+  },
+  defaultOptions: [],
+  create: context => {
+    return {
+      VariableDeclaration: node => {
+        if (node.kind === 'var') {
+          context.report({
+            node,
+              messageId: 'issue:var', // Prints the message with this ID when a problem is found
+              suggest: [
+                {
+                  messageId: 'fix:let',
+                  fix: fix: fixer => fixer.replaceText(node, 'let'),
+                },
+                {
+                  messageId: 'fix:const',
+                  fix: fix: fixer => fixer.replaceText(node, 'const'),
+                },
+              ],
+            })
+          }
+        },
+      };
+    },
+});
