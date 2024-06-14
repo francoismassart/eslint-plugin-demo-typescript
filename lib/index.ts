603df17a8bcdbeec545458b4ac32384697bb7db6
francoismassart <francois.massart@gmail.com>
2024-06-14 09:31:38 +0200

    fix: incorrect pathnames

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,7 @@ interface Plugin extends Omit<ESLint.Plugin, 'rules'> {
   rules: Record<RuleKey, RuleModule<any, any, any>>;
 }
 
-const { name, version } = require('../package.json') as {
+const { name, version } = require('../../package.json') as {
   name: string;
   version: string;
 };
