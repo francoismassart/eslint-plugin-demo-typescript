4ff09c14d16fbd3afe4ca183df2fae2be054bfb6
francoismassart <francois.massart@gmail.com>
2024-06-20 10:07:24 +0200

    fix: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['tests/**/*.spec.ts'],
+    include: ['src/**/*.spec.ts'],
     setupFiles: 'setup-vitest.js'
   },
 })
